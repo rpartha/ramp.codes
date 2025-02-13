3862b400e0eff11cfab0183d0dd343b979bc8d7a
Ramaseshan Parthasarathy <ram@Ramaseshans-Mac-mini.local>
2025-02-13 14:18:48 -0500

    refining the design

--- popular-point/tailwind.config.mjs
+++ popular-point/tailwind.config.mjs
@@ -3,36 +3,39 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
-      back: "#030d13",
+      back: "#rgba(0,1,35,1)",
       black: "#000",
       body: "rgba(0, 33, 33, 1)",
       gray: "#52525b",
       teal: "#004d4d",
       header: "#000808",
-      subText: "#002222",
+      subText: "#818181",
       infoBorder: "#96a9ff",
       infoIcon: "#96ddff",
       ideaIcon: "#ffed96",
       ideaBorder: "#ffe048",
       boxBG: "#000000",
       action: "#00274d",
-      copy: "#d5d8da",
-      logo: "linear-gradient(135deg, #004d4d 0%, #b6cccc 100%)",
+      link: "#000a3a",
+      diamond: "rgba(132, 132, 170, 1)",
+      homeBorder: "#c0c0c0",
+      pb: "linear-gradient(270deg, rgba(97,123,255,1) 0%, rgba(0,1,35,1) 100%)",
     },
     fontFamily: {
-      sans: ["Satoshi-Variable", "sans-serif"],
-      sansItalic: ["Satoshi-VariableItalic", "sans-serif"],
-      serif: ["goudy_old_style_pro-regular", "serif"],
-      serifBold: ["goudy_old_style_pro-bold", "serif"],
-      serifItalic: ["goudy_old_style_pro-italic", "serif"],
+      sans: ["PP Mori", "sans-serif"],
+      //serif: ["goudy_old_style_pro-regular", "serif"],
+      //serifBold: ["goudy_old_style_pro-bold", "serif"],
+      //serifItalic: ["goudy_old_style_pro-italic", "serif"],
+      serif: ["PP Eiko", "serif"],
       mono: ["dm", "monospace"],
-      body: ["__clean-custom"],
-      bodyItalic: ["__clean-custom"],
     },
     extend: {
       textShadow: {
         action:
           "-0.5px -0.5px 0 #000, 0.5px -0.5px 0 #000, -0.5px 0.5px 0 #000, 0.5px 0.5px 0 #000;",
+      },
+      screens: {
+        olg: "1080px", // Custom breakpoint for OnePlus 13
       },
     },
   },
@@ -44,13 +47,14 @@ export default {
             "-0.5px -0.5px 0 #000, 0.5px -0.5px 0 #000, -0.5px 0.5px 0 #000, 0.5px 0.5px 0 #000;",
         },
         ".gradient-text": {
-          backgroundImage: "linear-gradient(135deg, #004d4d 0%, #b6cccc 100%)",
+          backgroundImage:
+            "linear-gradient(270deg, rgba(97,123,255,1) 0%, rgba(0,1,35,1) 100%);",
           backgroundClip: "text",
           WebkitBackgroundClip: "text",
           color: "transparent", // Makes the gradient visible inside the text
           WebkitTextFillColor: "transparent",
           "-moz-background-clip": "text", // Firefox
-          textFillColor: "#004d4d", // Fallback solid color for Firefox
+          textFillColor: "rgba(0,1,35,1)", // Fallback solid color for Firefox
         },
       });
     },
